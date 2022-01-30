227689091f6bcb8d0dabd39f2d634599bf7f016b
daydy225 <dabelydadi@gmail.com>
2022-01-30 13:06:23 +0000

    premiere app update

--- lib/userpicscreen.dart
+++ lib/userpicscreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, duplicate_ignore
 
 import 'package:flutter/material.dart';
 
